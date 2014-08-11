fc8e6183965aeba59c550425f91f9fcae5cf36ce
tenny1028 <jasper@reddins-MBP-2011.local>
2014-08-10 21:38:49 -0500

    Finished product after youtube video 1
    I'm programming this over a series of 7-minute long videos, and each
    time I finish a video, I will commit my changes, each commit summary
    notes the name of the video in which I made all of the commits.

---
+++ MathQuizGame-Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MathQuizGame-Mac
+//
+//  Created by Jasper Reddin on 8/10/14.
+//  Copyright (c) 2014 TwoGuysBrickfilms. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MathQuizGame-Mac/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  MathQuizGame-Mac
+//
+//  Created by Jasper Reddin on 8/10/14.
+//  Copyright (c) 2014 TwoGuysBrickfilms. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
